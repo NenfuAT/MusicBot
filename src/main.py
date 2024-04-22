b97794f96ed9103060cfc71dd2f9da9afd0401e1
k21091 <k21091kk@aitech.ac.jp>
2024-04-22 15:17:58 +0900

    update:メッセージ式をスラッシュコマンドに変更

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 # 必要モジュールのインポート
 import discord
+from discord import app_commands
 import os
 
 TOKEN = os.environ['BOT_TOKEN']
@@ -8,22 +9,23 @@ TOKEN = os.environ['BOT_TOKEN']
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
-
+tree = app_commands.CommandTree(client)
 # 起動時に動作する処理
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
+    # アクティビティを設定 
+    new_activity = f"テスト" 
+    await client.change_presence(activity=discord.Game(new_activity)) 
+    # スラッシュコマンドを同期 
+    await tree.sync()
 
 # メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
+@tree.command(name='neko', description='猫が鳴きます')
+async def test(interaction: discord.Interaction):
+    await interaction.response.send_message('にゃーん')
+
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
